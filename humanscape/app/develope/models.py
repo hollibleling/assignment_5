4945931465ccd77f74564d94a496d725a67c7a35
Jungsoo Kim <hollibleling12@gmail.com>
2021-11-15 23:47:47 +0900

    feat: 모델링 및 db_uploader.py 완료

--- humanscape/app/develope/models.py
+++ humanscape/app/develope/models.py
@@ -18,7 +18,7 @@ class DevelopeList(models.Model):
     name = models.CharField(max_length=512)
     subject = models.CharField(max_length=128)
     agency = models.CharField(max_length=64)
-    target_count = models.PositiveIntegerField(default=0, null=True)
+    target_count = models.PositiveIntegerField(default=0, blank = True, null=True)
     period = models.CharField(max_length=16, null=True)
     kind = models.ForeignKey(Kind, on_delete=models.DO_NOTHING)
     phase = models.ForeignKey(Phase, on_delete=models.DO_NOTHING)
