6d42b05708501db0b5e9d66fb0a1a55347f1b23c
fall031 <fall900802@gmail.com>
2021-11-16 16:49:33 +0900

    feat: 임상정보 수집, 임상정보 검색 및 수집한 임상정보 리스트 API

--- humanscape/api/develope/views.py
+++ humanscape/api/develope/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import pagination, serializers
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+from datetime import datetime, timedelta
 
-# Create your views here.
+from .serializers import ResearchInformationSerializer
+from app.develope.models import ResearchInformation
+from api.pagination import CustomPagination
+
+class ResearchInformationViewset(ListModelMixin, RetrieveModelMixin, GenericViewSet):
+    queryset         = ResearchInformation.objects.all()
+    serializer_class = ResearchInformationSerializer
+    pagination_class = CustomPagination
+
+    def list(self, request, *args, **kwargs):
+        word = self.request.query_params.get('name', None)
+        if word:
+            queryset = self.get_queryset().filter(name__contains = word)
+        else:
+            queryset = self.get_queryset().filter(updated_at__range = [datetime.now()-timedelta(weeks=1), datetime.now()])
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
---
+++ humanscape/batchtask.py
@@ -0,0 +1,74 @@
+import urllib.request
+import json
+import django
+import os
+from datetime import datetime
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'humanscape.settings')
+django.setup()
+
+from app.develope.models import ResearchInformation
+
+def BatchTask():
+    print("====================================")
+    print("Success")
+    print(datetime.now())
+    encodingKey = "SfO9P5fMenC%2BsllZjDwBGoPlKepb7sGa%2BGHv2hnt%2Bdhe76QQ3ntEnIH3hajnGkjJTC6CqqNLvEWd9xWh7QE49A%3D%3D"
+    decodingKey = "SfO9P5fMenC+sllZjDwBGoPlKepb7sGa+GHv2hnt+dhe76QQ3ntEnIH3hajnGkjJTC6CqqNLvEWd9xWh7QE49A=="
+
+    url = "https://api.odcloud.kr/api/3074271/v1/uddi:cfc19dda-6f75-4c57-86a8-bb9c8b103887?page=1&perPage=1000&serviceKey=" + encodingKey
+
+    response = urllib.request.urlopen(url)
+
+    json_str = response.read().decode("utf-8")
+    json_object = json.loads(json_str)
+    data = json_object['data']
+
+    for i in range(len(data)):
+        research = ResearchInformation.objects.all()
+        if not research.filter(number=data[i]['과제번호']):
+            if not data[i]['전체목표연구대상자수'] =='':
+                subject_number = int(data[i]['전체목표연구대상자수'])
+            else: subject_number = None
+            ResearchInformation.objects.create(
+                name = data[i]['과제명'],
+                number = data[i]['과제번호'],
+                period = data[i]['연구기간'],
+                scope = data[i]['연구범위'],
+                kind = data[i]['연구종류'],
+                institute = data[i]['연구책임기관'],
+                phase = data[i]['임상시험단계(연구모형)'],
+                subject_number = subject_number,
+                department = data[i]['진료과']
+            )
+        
+        else:
+            research = research.get(number=data[i]['과제번호'])
+            if not data[i]['전체목표연구대상자수'] == '':
+                subject_number = int(data[i]['전체목표연구대상자수'])
+            else: subject_number = None
+
+            if not research.name == data[i]['과제명']:
+                research.name = data[i]['과제명']
+                research.save()
+            if not research.period == data[i]['연구기간']:
+                research.period = data[i]['연구기간']
+                research.save()
+            if not research.scope == data[i]['연구범위']:
+                research.scaop = data[i]['연구범위']
+                research.save()
+            if not research.kind == data[i]['연구종류']:
+                research.kind = data[i]['연구종류']
+                research.save()
+            if not research.institute == data[i]['연구책임기관']:
+                research.institute = data[i]['연구책임기관']
+                research.save()
+            if not research.phase == data[i]['임상시험단계(연구모형)']:
+                research.phase = data[i]['임상시험단계(연구모형)']
+                research.save()
+            if not research.subject_number == subject_number:
+                research.subject_number = subject_number
+                research.save()
+            if not research.department == data[i]['진료과']:
+                research.department = data[i]['진료과']
+                research.save()
