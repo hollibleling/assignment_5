327ebe553c7d9a8b9d3a31676369b3952f2cba3d
YusunL <47683668+YusunL@users.noreply.github.com>
2021-11-16 11:26:47 +0900

    feat: 수집한 임상정보 리스트 api

--- humanscape/api/develope/views.py
+++ humanscape/api/develope/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import ResearchDataSerializer
+from app.develope.models import ResearchData
+
+from datetime import datetime, timedelta
+
+
+import csv
+
+
+
+
+class ResearchDataViewSet(ListModelMixin, CreateModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = ResearchData.objects.all()
+    serializer_class = ResearchDataSerializer
+    permission_classes = [AllowAny]
+
+
+    def get_queryset(self, updated, title, inst, research_id):
+        returnData = ResearchData.objects.filter(title=title, institution=inst, research_id=research_id )
+        if updated == '1':
+            returnData.filter(updated_at__gte=datetime.now()-timedelta(days=7))
+        return returnData
+
+    def list(self, request, *args, **kwargs):
+        updated = self.request.query_params.get('updated_only','0')
+        title = self.request.query_params.get('title','')
+        inst = self.request.query_params.get('inst','')
+        research_id = self.request.query_params.get('id', '')
+        queryset = self.get_queryset(updated, title, inst, research_id)
+        page =self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer_class(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    # def import_csv(self):
+    #     with open('C:/Users/imyur/Documents/GitHub/assignment_5/data.csv','r')as f:
+    #         dr = csv.DictReader(f)
--- humanscape/app/develope/models.py
+++ humanscape/app/develope/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class ResearchData(models.Model):
+    research_id = models.CharField(max_length=7, unique=True)
+    title = models.CharField(max_length=50)
+    department = models.CharField(max_length=15)
+    institution = models.CharField(max_length=15)
+    subjects_num = models.CharField(max_length=6, blank=True)
+    period = models.CharField(max_length=10, blank=True)
+    sort = models.CharField(max_length=10)
+    stage = models.CharField(max_length=10, blank=True)
+    range = models.CharField(max_length=10)
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
